d648ea25883b9db37b685b750a31d1c3ed373250
Ayush Mishra <ayush2444666666@gmail.com>
2026-08-08 14:48:36 +0530

    Import cron job module in index.js for scheduled tasks

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT;
