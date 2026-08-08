dbadb40db049d7d669182f075e672e776b0ecf0c
Ayush Mishra <ayush2444666666@gmail.com>
2026-08-08 20:15:54 +0530

    Fix import path for Clerk webhook module in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
-import clerkWebhook from "./webhooks/clerk.webhooks.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 const PORT = process.env.PORT;
